12303e0fe901f3724d58cdf3707e377fff37e423
LEONARDO REIS <pereiraleonardo139@gmail.com>
2025-03-06 19:30:03 -0300

    teste

---
+++ script.js.js
@@ -0,0 +1,23 @@
+function adicionarTarefa() {
+
+    let mensagem = "Tarefa adicionada com sucesso!";
+    
+    let inputTarefa = document.getElementById("inputTarefa")
+    let tarefa = inputTarefa.value 
+    document.getElementById("mensagem").textContent = mensagem;
+
+    let lisTaTarefas = document.getElementById("listaTarefas")
+     let novaTarefa = document.createElement("li")
+
+    novaTarefa.textContent = tarefa
+
+
+     listaTarefas.appendChild(novaTarefa)
+
+     
+
+
+    inputTarefa.value = ""
+
+
+  }
